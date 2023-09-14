d16d09eb7af4c84a0cea88562a31acc7e3f63aa7
bimaafrizal <bimaafrizalmalna@student.uns.ac.id>
2023-09-14 23:00:23 +0700

    update channel

--- Goroutines/channel_test.go
+++ Goroutines/channel_test.go
@@ -2,6 +2,7 @@ package Goroutines
 
 import (
 	"fmt"
+	"strconv"
 	"testing"
 	"time"
 )
@@ -63,6 +64,7 @@ func OnlyIn(channel chan<- string) {
 	time.Sleep(2 * time.Second)
 	channel <- "Bima Afrizal" //mengirim data ke channel
 }
+
 func OnlyOut(channel <-chan string) {
 	data := <-channel //menerima data dari channel
 	fmt.Println(data)
@@ -76,4 +78,82 @@ func TestInOutChannel(t *testing.T) {
 	go OnlyOut(channel)
 
 	time.Sleep(5 * time.Second)
+}
+
+// buffered channel
+// kadang ads kasus dimana pengirim lebih cepat dari penerima, maka otomatis pengirim akan ikut lambat
+// buffered channel adalah channel yang bisa figunakan untuk menampung data antrian
+// buffer capacity => jumlah data yang bisa ditampung dalam antrian
+// jika buffer penuh maka data akan diminta menunggu
+func TestBufferedChannel(t *testing.T) {
+	channel := make(chan string, 3) //membuat channel dengan tipe data string dan buffer 3
+	defer close(channel)            //menutup channel
+
+	go (func() {
+		channel <- "Bima"
+		channel <- "Afrizal"
+		channel <- "Malnaa"
+	})()
+
+	go (func() {
+		fmt.Println(<-channel)
+		fmt.Println(<-channel)
+		fmt.Println(<-channel)
+	})()
+
+	time.Sleep(2 * time.Second)
+	fmt.Println("selesai")
+}
+
+// range cahnnel
+// untuk kasus dimana kita mengirim data secara terus menerus ke channel dan tidak tau kapan berhenti
+func TestRangeChannel(t *testing.T) {
+	channel := make(chan string)
+
+	go func() {
+		for i := 1000; i < 100000; i++ {
+			channel <- "Perulangan ke " + strconv.Itoa(i)
+		}
+		close(channel)
+	}()
+
+	//perulangannya akan berhenti ketika channelnya di close
+	for data := range channel {
+		fmt.Println("menerima data", data)
+	}
+
+	fmt.Println("selesai")
+}
+
+// select channel
+// mengambil data dari channel sekaligus
+// digunakan untuk mendapatkan data tercepat dari beberapa channel yang berjalan bersamaan, jika bersamaan maka akan dipilih secara random
+func TestSelectChannel(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+	defer close(channel1)
+	defer close(channel2)
+
+	go GiveMeRespon(channel1)
+	go GiveMeRespon(channel2)
+
+	counter := 0
+	for {
+		select {
+		case data := <-channel1:
+			fmt.Println("Data dari channel 1", data)
+			counter++
+		case data := <-channel2:
+			fmt.Println("Data dari channel 2", data)
+			counter++
+		// default select channel
+		// digunakan untuk menghindari deadlock karena data channel tidak ada
+		// akan dieksekusi jika tidak ada data di channel
+		default:
+			fmt.Println("Menunggu data")
+		}
+		if counter == 2 {
+			break
+		}
+	}
 }
